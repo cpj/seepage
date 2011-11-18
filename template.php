a8ee2c06b869b3222c41a8b1ed8f17c648bed016
Seth Vincent <sethvincent@gmail.com>
2011-11-18 18:52:47 +0000

    html5, rdf doctypes

---
+++ template.php
@@ -0,0 +1,18 @@
+/**
+ * Uses RDFa attributes if the RDF module is enabled
+ * Lifted from Adaptivetheme for D7, full credit to Jeff Burnz
+ * ref: http://drupal.org/node/887600
+ */
+function seepage_preprocess_html(&$vars) {
+  if (module_exists('rdf')) {
+    $vars['doctype'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">' . "\n";
+    $vars['rdf']->version = 'version="HTML+RDFa 1.1"';
+    $vars['rdf']->namespaces = $vars['rdf_namespaces'];
+    $vars['rdf']->profile = ' profile="' . $vars['grddl_profile'] . '"';
+  } else {
+    $vars['doctype'] = '<!DOCTYPE html>' . "\n";
+    $vars['rdf']->version = '';
+    $vars['rdf']->namespaces = '';
+    $vars['rdf']->profile = '';
+  }
+}
